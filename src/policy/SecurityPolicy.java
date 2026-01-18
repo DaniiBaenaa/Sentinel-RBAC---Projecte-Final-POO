0e1ea7608e01349c2840b4ad9f803f7550ab8403
DaniiBaenaa <daniel.baena@enti.cat>
2026-01-18 18:49:05 +0100

    Acabo classe security policy

--- src/policy/SecurityPolicy.java
+++ src/policy/SecurityPolicy.java
@@ -1,5 +1,15 @@
 package policy;
 
-public class SecurityPolicy {
-    
+import model.AccessRequest;
+
+public abstract class SecurityPolicy {
+    protected String nom;
+    protected boolean activa;
+
+    public SecurityPolicy(String nom, boolean activa) {
+        this.nom = nom;
+        this.activa = activa;
+    }
+
+    public abstract boolean evaluate(AccessRequest peticio, String ip);
 }
