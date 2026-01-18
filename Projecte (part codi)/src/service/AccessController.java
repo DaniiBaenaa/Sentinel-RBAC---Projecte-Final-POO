f9054782aac2946f3a2a6e3b685db891137ef414
cobosenti <joan.cobos@estudiant.enti.cat>
2026-01-18 19:16:43 +0100

    Començament AccesController

--- Projecte (part codi)/src/service/AccessController.java
+++ Projecte (part codi)/src/service/AccessController.java
@@ -1,5 +1,38 @@
 package service;
 
+import model.AccessRequest;
+import model.Resource;
+import model.User;
+import model.enums.ClassificationLevel;
+import model.enums.RoleType;
+import policy.SecurityPolicy;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccessController {
-    
-}
+
+    private List<SecurityPolicy> politiques = new ArrayList<SecurityPolicy>();
+
+    public void setPolicies(List<SecurityPolicy> policies) {
+        this.politiques = new ArrayList<SecurityPolicy>(policies);
+    }
+
+    public boolean verifyPermission(User usuari, Resource recurs) {
+        RoleType rol = usuari.getRole();
+        ClassificationLevel nivell = recurs.getSensitivity();
+
+        if (rol == RoleType.ADMIN) {
+            return true;
+        }
+
+        if (rol == RoleType.EMPLOYEE) {
+            return nivell == ClassificationLevel.PUBLIC || nivell == ClassificationLevel.INTERNAL;
+        }
+
+        if (rol == RoleType.GUEST) {
+            return nivell == ClassificationLevel.PUBLIC;
+        }
+
+        return false;
+    }
