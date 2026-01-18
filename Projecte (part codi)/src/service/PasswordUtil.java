3d9d71df60792f7b0163eccda09202657e654c57
Rocreverte <roc.reverte@enti.cat>
2026-01-18 21:17:22 +0100

    Creacio de la classe PasswordUtil
    Sirve para el login.

--- Projecte (part codi)/src/service/PasswordUtil.java
+++ Projecte (part codi)/src/service/PasswordUtil.java
@@ -1,5 +1,23 @@
 package service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
 public class PasswordUtil {
-    
+
+    public static String sha256(String text) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] sortida = md.digest(text.getBytes(StandardCharsets.UTF_8));
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : sortida) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error generant hash");
+        }
+    }
 }
