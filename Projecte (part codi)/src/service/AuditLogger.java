67e03e978c969e61435bfd170cd9477daa1c30ec
DaniiBaenaa <daniel.baena@enti.cat>
2026-01-18 19:48:36 +0100

    Acabar la classe auditlogger

--- Projecte (part codi)/src/service/AuditLogger.java
+++ Projecte (part codi)/src/service/AuditLogger.java
@@ -27,4 +27,45 @@ public class AuditLogger {
         afegirLinia("[INFO] " + missatge + "\n");
     }
 
+    public void logAccess(AccessRequest peticio, String ip, boolean rolOk, boolean politiquesOk) {
+        String linia = "[ACCESS] usuari=" + peticio.getRequestor().getUsername()
+                + " rol=" + peticio.getRequestor().getRole()
+                + " recurs=" + peticio.getTarget().getResourceId()
+                + " ip=" + ip
+                + " rolOk=" + rolOk
+                + " politiquesOk=" + politiquesOk
+                + " estat=" + peticio.getStatus()
+                + "\n";
+
+        afegirLinia(linia);
+    }
+
+    private void afegirLinia(String text) {
+        try {
+            Files.writeString(ruta, text, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.err.println("Error escrivint al log: " + e.getMessage());
+        }
+    }
+
+    public String readTail(int maxLinies) {
+        try {
+            List<String> linies = Files.readAllLines(ruta);
+
+            int inici = 0;
+            if (linies.size() > maxLinies) {
+                inici = linies.size() - maxLinies;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = inici; i < linies.size(); i++) {
+                sb.append(linies.get(i)).append("\n");
+            }
+            return sb.toString();
+
+        } catch (IOException e) {
+            return "No s'ha pogut llegir el log";
+        }
+    }
 }
+
